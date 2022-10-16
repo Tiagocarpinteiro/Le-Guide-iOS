d2f4e7c682699c413157a52f1e3f9832c1673001
Tiago Carpinteiro <tiago.carpinteiro07@gmal.com>
2022-10-16 17:12:00 +0200

    Initial Commit

---
+++ Le Guide/Le_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Le_GuideApp.swift
+//  Le Guide
+//
+//  Created by Tiago Carpinteiro on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Le_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
