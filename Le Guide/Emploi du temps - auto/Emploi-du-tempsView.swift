ea96275b5b5c0a9d63aa3ca7005364b95a17b926
Tiagocarpinteiro <tiago.carpinteiro07@gmail.com>
2023-03-22 17:50:39 -0400

    refonte de l'application

---
+++ Le Guide/Emploi du temps - auto/Emploi-du-tempsView.swift
@@ -0,0 +1,20 @@
+//
+//  Emploi-du-tempsView.swift
+//  Le Guide
+//
+//  Created by Tiago Carpinteiro on 01/12/2022.
+//
+
+import SwiftUI
+
+struct Emploi_du_tempsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Emploi_du_tempsView_Previews: PreviewProvider {
+    static var previews: some View {
+        Emploi_du_tempsView()
+    }
+}
